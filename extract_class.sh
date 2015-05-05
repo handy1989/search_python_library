08b5f9ea174f5128f520b458032d05eaabf4b485
LittleWhite <menghan1218@gmail.com>
2015-05-05 11:27:40 +0800

    filter object which is not class or function

--- extract_class.sh
+++ extract_class.sh
@@ -1,5 +1,5 @@
 ls ./library | while read file
 do
     class_name=${file%.html}
-    grep -Eo "id=\"[^\"]*\"" ./library/$file | awk -v class=$class_name -F'"' '{print class"\t"$2}'
+    grep -Eo "id=\"[^\"]*\"" ./library/$file | awk -v class=$class_name -F'"' '{print class"\t"$2}' | grep -v '-'
 done > ./class_and_functions.txt
